254596ecaa31e17b444ff815ece54e408a582917
Claudiu <claudiu@coderollers.com>
2021-10-27 17:11:14 +0200

    Remove error parameter (#3)

--- env.go
+++ env.go
@@ -12,28 +12,25 @@ func EnvOrDefault(name, def string) string {
 	return def
 }
 
-func EnvOrDefaultInt64(name string, def int64, err *error) int64 {
+func EnvOrDefaultInt64(name string, def int64) int64 {
 	if v, ok := os.LookupEnv(name); ok {
-		vc, e := strconv.ParseInt(v, 10, 32)
-		err = &e
+		vc, _ := strconv.ParseInt(v, 10, 32)
 		return vc
 	}
 	return def
 }
 
-func EnvOrDefaultInt32(name string, def int32, err *error) int32 {
+func EnvOrDefaultInt32(name string, def int32) int32 {
 	if v, ok := os.LookupEnv(name); ok {
-		vc, e := strconv.ParseInt(v, 10, 32)
-		err = &e
+		vc, _ := strconv.ParseInt(v, 10, 32)
 		return int32(vc)
 	}
 	return def
 }
 
-func EnvOrDefaultBool(name string, def bool, err *error) bool {
+func EnvOrDefaultBool(name string, def bool) bool {
 	if v, ok := os.LookupEnv(name); ok {
-		vc, e := strconv.ParseBool(v)
-		err = &e
+		vc, _ := strconv.ParseBool(v)
 		return vc
 	}
 	return def
