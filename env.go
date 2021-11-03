2e515cc3000b50c791a92493010313266d89da4b
Claudiu <claudiu@coderollers.com>
2021-11-03 22:37:53 +0100

    Add EnvOrDefaultStringSlice and comments (#4)

--- env.go
+++ env.go
@@ -3,8 +3,13 @@ package utils
 import (
 	"os"
 	"strconv"
+	"strings"
 )
 
+//EnvOrDefault returns the value of the environment variable identified by name parameter, or
+//the value of the def parameter if the environment variable is missing.
+//
+//Note: It will return an empty string if the environment variable is set to an empty string.
 func EnvOrDefault(name, def string) string {
 	if v, ok := os.LookupEnv(name); ok {
 		return v
@@ -12,6 +17,10 @@ func EnvOrDefault(name, def string) string {
 	return def
 }
 
+//EnvOrDefaultInt64 returns the value of the environment variable identified by name parameter
+//cast to a 64bit decimal integer, or the value of the def parameter if the environment variable is missing or empty.
+//
+//Note: It will return the value of def if the environment variable is set to an empty string.
 func EnvOrDefaultInt64(name string, def int64) int64 {
 	if v, ok := os.LookupEnv(name); ok {
 		vc, _ := strconv.ParseInt(v, 10, 32)
@@ -20,6 +29,10 @@ func EnvOrDefaultInt64(name string, def int64) int64 {
 	return def
 }
 
+//EnvOrDefaultInt32 returns the value of the environment variable identified by name parameter
+//cast to a 32bit decimal integer, or the value of the def parameter if the environment variable is missing or empty.
+//
+//Note: It will return the value of def if the environment variable is set to an empty string.
 func EnvOrDefaultInt32(name string, def int32) int32 {
 	if v, ok := os.LookupEnv(name); ok {
 		vc, _ := strconv.ParseInt(v, 10, 32)
@@ -28,10 +41,32 @@ func EnvOrDefaultInt32(name string, def int32) int32 {
 	return def
 }
 
+//EnvOrDefaultBool returns the value of the environment variable identified by name parameter
+//cast to a boolean, or the value of the def parameter if the environment variable is missing or empty.
+//
+//Note: It will return the value of def if the environment variable is set to an empty string.
 func EnvOrDefaultBool(name string, def bool) bool {
 	if v, ok := os.LookupEnv(name); ok {
 		vc, _ := strconv.ParseBool(v)
 		return vc
+	}
+	return def
+}
+
+//EnvOrDefaultStringSlice returns the value of the environment variable identified by name parameter split by the value
+//of the separator parameter as a slice of strings, or the value of the def parameter if the environment variable is missing.
+//
+//Note: It will return an empty slice if the environment variable is set to an empty string.
+func EnvOrDefaultStringSlice(name, separator string, def []string) []string {
+	if v, ok := os.LookupEnv(name); ok {
+		var result []string
+		for _, substr := range strings.Split(v, separator) {
+			trimmed := strings.TrimSpace(substr)
+			if trimmed == "" {
+				continue
+			}
+			result = append(result, trimmed)
+		}
 	}
 	return def
 }
--- http_error.go
+++ http_error.go
@@ -5,12 +5,14 @@ import (
 	"github.com/go-errors/errors"
 )
 
+//HttpError defines the generic structure of an HTTP error response.
 type HttpError struct {
 	Code    int
 	Err     error
 	Message string
 }
 
+//Error will return the string representation of the HttpError.Message and Err, omitting the former if it is not set.
 func (err *HttpError) Error() string {
 	if err.Message != "" {
 		return fmt.Sprintf("%s: %s", err.Message, err.Err.Error())
@@ -18,10 +20,12 @@ func (err *HttpError) Error() string {
 	return err.Err.Error()
 }
 
+//StackTrace will return a string representation of Err's stack trace
 func (err *HttpError) StackTrace() string {
 	return errors.Wrap(err.Err, 1).ErrorStack()
 }
 
+//ErrorAndStack will return both the output of Error and StackTrace in one call
 func (err *HttpError) ErrorAndStack() (string, string) {
 	return err.Error(), err.StackTrace()
 }
